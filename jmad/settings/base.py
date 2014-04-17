3545b421530d95f8fb55ec6c2e0e3e937eded992
kcharvey <kcharvey@gmail.com>
2014-04-17 00:39:37 -0400

    seperate dev settings file, add django-tdd

--- jmad/settings/base.py
+++ jmad/settings/base.py
@@ -19,11 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
---
+++ jmad/settings/dev.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_tdd',
+)
+
