3831ca21e6ab0e9dd1b76af136a520695731f772
kcharvey <kcharvey@gmail.com>
2014-04-24 14:33:06 -0400

    back got django-jenkins 0.15.0, try setting PYLINT_RCFILE value

--- jmad/settings/ci.py
+++ jmad/settings/ci.py
@@ -23,9 +23,10 @@ INSTALLED_APPS = (
 )
 
 JENKINS_TASKS = (
-    'django_jenkins.tasks.django_tests',
     'django_jenkins.tasks.run_pylint',
     'django_jenkins.tasks.with_coverage',
     'django_jenkins.tasks.run_pep8',
 )
+
+PYLINT_RCFILE = os.path.join(BASE_DIR, 'settings', 'pylint.rc')
 
